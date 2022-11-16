fe4d080a215f435682874a58a135ca4b4bdd6328
= <tiago.polemarxgroup@gmail.com>
2022-11-16 18:34:21 -0300

    Projeto criado

---
+++ aposta/src/test/java/com/polemarxgroup/aposta/ApostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polemarxgroup.aposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
